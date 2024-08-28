b1654db7b891c2e6b2f1a3f4cada5b678cfdce5b
tasosger <tassos.gerassis@gmail.com>
2024-08-28 15:29:09 +0300

    allow local host 3000

--- src/main/java/com/uninote/backend/config/WebSocketConfig.java
+++ src/main/java/com/uninote/backend/config/WebSocketConfig.java
@@ -19,6 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*").withSockJS();
+        .setAllowedOrigins("http://localhost:3000", "https://uninote.gr").withSockJS();
     }
 }
