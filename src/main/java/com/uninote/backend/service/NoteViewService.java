e06dda39f2277672ac361077890f393aa1c0e923
tasosger <tassos.gerassis@gmail.com>
2024-08-29 03:22:33 +0300

    VIEW INCREASE LOGS

--- src/main/java/com/uninote/backend/service/NoteViewService.java
+++ src/main/java/com/uninote/backend/service/NoteViewService.java
@@ -52,7 +52,7 @@ public class NoteViewService {
             noteView.setUserId(userId);
             noteView.setCreatedAt(LocalDateTime.now());; 
         
-
+            userService.updateUniScore(note.getUser(), 3L);
        
         return noteViewRepository.save(noteView);
     }
