3fc8a83c4b69886e0887ac382d5e2e3b3eb6fae7
tasosger <tassos.gerassis@gmail.com>
2024-09-01 12:35:31 +0300

    authorize only corect vcel app

--- src/main/java/com/uninote/backend/config/WebConfig.java
+++ src/main/java/com/uninote/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://uninote.gr", "https://uninote-creators-portal-eiz6j2q68-uninotes-projects.vercel.app","https://uninote-creators-portal.vercel.app/")  
+                .allowedOrigins("http://localhost:3000", "https://uninote.gr","https://uninote-creators-portal.vercel.app/")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
