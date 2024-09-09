22778f87808fe3fb8ef80d5fa7e7199fe93f2e62
tasosger <tassos.gerassis@gmail.com>
2024-09-09 17:22:51 +0300

    user weekly logins

--- src/main/java/com/uninote/backend/repository/UserLoginRepository.java
+++ src/main/java/com/uninote/backend/repository/UserLoginRepository.java
@@ -15,4 +15,13 @@ public interface UserLoginRepository extends JpaRepository<UserLogin, Long> {
     // Find users who have logged in on two consecutive days
     @Query("SELECT u.user.id FROM UserLogin u WHERE u.loginTimestamp >= :start AND u.loginTimestamp < :end GROUP BY u.user.id HAVING COUNT(DISTINCT TRUNC(u.loginTimestamp)) = 2")
     List<Long> findUsersLoggedInConsecutively(LocalDateTime start, LocalDateTime end);
+
+
+    @Query(value = "SELECT ul.USER_ID, COUNT(DISTINCT TRUNC(ul.LOGIN_TIMESTAMP)) " +
+                   "FROM USER_LOGINS ul " +
+                   "WHERE ul.LOGIN_TIMESTAMP BETWEEN :startDate AND :endDate " +
+                   "GROUP BY ul.USER_ID", 
+           nativeQuery = true)
+    List<Object[]> findUserDistinctLoginDaysBetweenDates(LocalDateTime startDate, LocalDateTime endDate);
+
 }   
--- src/main/java/com/uninote/backend/service/UserLoginService.java
+++ src/main/java/com/uninote/backend/service/UserLoginService.java
@@ -8,6 +8,8 @@ import com.uninote.backend.repository.UserLoginRepository;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class UserLoginService {
@@ -34,5 +36,20 @@ public class UserLoginService {
 
     public List<Long> getUsersLoggedInConsecutively(LocalDateTime start, LocalDateTime end) {
         return userLoginRepository.findUsersLoggedInConsecutively(start, end);
+    }
+
+
+    public Map<Integer, Long> getUserDistinctLoginDays(LocalDateTime startDate, LocalDateTime endDate) {
+        
+        List<Object[]> userDistinctLoginDays = userLoginRepository.findUserDistinctLoginDaysBetweenDates(startDate, endDate);
+
+        
+        Map<Integer, Long> loginDayCountMap = userDistinctLoginDays.stream()
+            .collect(Collectors.groupingBy(
+                row -> ((Number) row[1]).intValue(),  
+                Collectors.counting()                 
+            ));
+
+        return loginDayCountMap;
     }
 }
