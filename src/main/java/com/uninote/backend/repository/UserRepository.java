97aca856464ad0cf666759a8d5276365bb616c51
tasosger <tassos.gerassis@gmail.com>
2024-09-17 17:01:49 +0300

    small fix

--- src/main/java/com/uninote/backend/repository/UserRepository.java
+++ src/main/java/com/uninote/backend/repository/UserRepository.java
@@ -53,7 +53,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     //Optional<User> findByFirebaseUid(String firebaseUid);
 
-    @Query(value = "SELECT user_id FROM users  WHERE firebase_uid = :firebaseUid", nativeQuery = true)
+    @Query(value = "SELECT user_id FROM admin.users  WHERE firebase_uid = :firebaseUid", nativeQuery = true)
     Optional<Long> findUserIdByFirebaseUid(@Param("firebaseUid") String firebaseUid);
 
     @Query("SELECT COUNT(n) FROM Note n WHERE n.user.id = :userId")
