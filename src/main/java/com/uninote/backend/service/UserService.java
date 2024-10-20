87268617aa53e48ef17d25827549673b1e76e15e
tasosger <tassos.gerassis@gmail.com>
2024-10-20 22:37:19 +0300

    concurrency map

--- src/main/java/com/uninote/backend/service/UserService.java
+++ src/main/java/com/uninote/backend/service/UserService.java
@@ -50,6 +50,7 @@ import java.util.Optional;
 import java.util.PrimitiveIterator;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 
@@ -120,65 +121,78 @@ public class UserService {
     @Autowired
     private SeasonService seasonService;
 
+
+    private final Map<Long, Object> locks = new ConcurrentHashMap<>();
+
+
     public Long loginUserAndUpdateStreak(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + userId));
 
-        LocalDate lastLoginDate = (user.getLastLogin() != null) ? user.getLastLogin().toLocalDate() : null;
-        LocalDate today = LocalDate.now();
+        locks.putIfAbsent(userId, new Object());
+        synchronized (locks.get(userId)) {
+            try {
 
+                User user = userRepository.findById(userId)
+                        .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + userId));
 
-        boolean isFirstLogin = (lastLoginDate == null || lastLoginDate.isBefore(today.minusDays(1)));
-        boolean isConsecutiveLogin = (lastLoginDate != null && lastLoginDate.isEqual(today.minusDays(1)));
-
-
-        if (isFirstLogin) {
-            user.setStreak(1);
-            updateUniScore(user, 24L);
-        } else if (isConsecutiveLogin) {
-            user.setStreak(user.getStreak() + 1);
-            updateUniScore(user, 24L);
-            updateUniScore(user, 25L);
-        }
+                LocalDate lastLoginDate = (user.getLastLogin() != null) ? user.getLastLogin().toLocalDate() : null;
+                LocalDate today = LocalDate.now();
 
 
-        user.setLastLogin(LocalDateTime.now());
-        CompletableFuture<Void> notificationFuture = CompletableFuture.runAsync(()-> {
-            if (isFirstLogin || isConsecutiveLogin) {
-                loginWebSocketController.sendLoginNotification(
-                        user.getFirebaseUid(),
-                        "Congratulations! You have received 50 uniscore for logging in today."
-                );
+                boolean isFirstLogin = (lastLoginDate == null || lastLoginDate.isBefore(today.minusDays(1)));
+                boolean isConsecutiveLogin = (lastLoginDate != null && lastLoginDate.isEqual(today.minusDays(1)));
+
+
+                if (isFirstLogin) {
+                    user.setStreak(1);
+                    updateUniScore(user, 24L);
+                } else if (isConsecutiveLogin) {
+                    user.setStreak(user.getStreak() + 1);
+                    updateUniScore(user, 24L);
+                    updateUniScore(user, 25L);
+                }
+
+
+                user.setLastLogin(LocalDateTime.now());
+                CompletableFuture<Void> notificationFuture = CompletableFuture.runAsync(()-> {
+                    if (isFirstLogin || isConsecutiveLogin) {
+                        loginWebSocketController.sendLoginNotification(
+                                user.getFirebaseUid(),
+                                "Congratulations! You have received 50 uniscore for logging in today."
+                        );
+                    }
+                });
+                userRepository.save(user);
+
+                CompletableFuture<Void> badgeFuture = CompletableFuture.runAsync(() -> {
+                    badgeService.checkBadgesForUser(userId);
+
+                });
+
+                UserLogin userLogin = new UserLogin();  
+                userLogin.setUser(user);
+                userLogin.setLoginTimestamp(LocalDateTime.now());
+                userLoginRepository.save(userLogin);
+                Optional<UserSession> activeSession = userSessionRepository.findActiveSessionByUserId(userId);
+                Long sessionId;
+                if (activeSession.isPresent()) {
+                
+                    sessionId = activeSession.get().getSessionId();
+                } else {
+                    sessionId = userSessionService.startSession(userId);
+                }
+
+
+                CompletableFuture<Void> allTasks = CompletableFuture.allOf(notificationFuture,badgeFuture);
+                allTasks.exceptionally(ex -> {
+                    System.err.println("An error occurred during asynchronous operations: " + ex.getMessage());
+                    return null;
+                });
+                //allTasks.join();
+                return sessionId;
+            } finally {
+                locks.remove(userId);
             }
-        });
-        userRepository.save(user);
-
-        CompletableFuture<Void> badgeFuture = CompletableFuture.runAsync(() -> {
-            badgeService.checkBadgesForUser(userId);
-
-        });
-
-        UserLogin userLogin = new UserLogin();  
-        userLogin.setUser(user);
-        userLogin.setLoginTimestamp(LocalDateTime.now());
-        userLoginRepository.save(userLogin);
-        Optional<UserSession> activeSession = userSessionRepository.findActiveSessionByUserId(userId);
-        Long sessionId;
-        if (activeSession.isPresent()) {
-        
-            sessionId = activeSession.get().getSessionId();
-        } else {
-            sessionId = userSessionService.startSession(userId);
         }
-
-
-        CompletableFuture<Void> allTasks = CompletableFuture.allOf(notificationFuture,badgeFuture);
-        allTasks.exceptionally(ex -> {
-            System.err.println("An error occurred during asynchronous operations: " + ex.getMessage());
-            return null;
-        });
-        //allTasks.join();
-        return sessionId;
     }
 
     public User findById(Long userId) {
