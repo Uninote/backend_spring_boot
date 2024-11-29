4d3220cbc24547028ffbd190bef584d55feed934
tasosger <tassos.gerassis@gmail.com>
2024-11-29 10:28:53 +0200

    register bounty castle

--- src/main/java/com/uninote/backend/DemoApplication.java
+++ src/main/java/com/uninote/backend/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.uninote.backend;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.Security;
 
 @SpringBootApplication
 //@EnableCaching
 @EnableAsync
 public class DemoApplication {
     public static void main(String[] args) {
+        Security.addProvider(new BouncyCastleProvider());
         SpringApplication.run(DemoApplication.class, args);
     }
 }
