112fa815be6bcfb7b4c8c77677013d4c04870ce3
tasosger <tassos.gerassis@gmail.com>
2025-01-05 13:22:50 +0200

    controller for note processing

---
+++ src/main/java/com/uninote/backend/controller/TutieController.java
@@ -0,0 +1,13 @@
+package com.uninote.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tutie")
+public class TutieController {
+    
+}
