3a6ff0fd8072220ac6c739e588c0314e43fd4879
tasosger <tassos.gerassis@gmail.com>
2025-01-17 00:13:05 +0200

    lazy init

--- src/main/java/com/uninote/backend/DemoApplication.java
+++ src/main/java/com/uninote/backend/DemoApplication.java
@@ -3,13 +3,17 @@ package com.uninote.backend;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.security.Security;  
 
-@SpringBootApplication(scanBasePackages = "com.uninote.backend")
+
+@SpringBootApplication(scanBasePackages = "com.uninote.backend",exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
+
 //@EnableCaching
 @EnableAsync
 public class DemoApplication {
