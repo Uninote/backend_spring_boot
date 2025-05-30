22fc2ce12a61f3b9d792d48c85c3b9b61a3a47bd
Tasos Gerasis <147561234+tasosger@users.noreply.github.com>
2025-05-30 14:08:45 +0300

    Update DemoApplication.java

--- src/main/java/com/uninote/backend/DemoApplication.java
+++ src/main/java/com/uninote/backend/DemoApplication.java
@@ -10,24 +10,26 @@ import com.uninote.backend.config.StripeConfig; // Add this import
 
 
 @SpringBootApplication
-
-//@EnableCaching
 @EnableAsync
 public class DemoApplication {
     public static void main(String[] args) {
-         String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+        String profile = System.getenv("SPRING_PROFILES_ACTIVE");
 
         if (profile == null || profile.isEmpty()) {
             profile = "development";
         }
 
-        if ("development".equalsIgnoreCase(profile)) {
-            Dotenv dotenv = Dotenv.configure()
-                    .ignoreIfMissing()
-                    .load();
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
-        } 
-        //Security.addProvider(new BouncyCastleProvider());
+        // manually inject dotenv into system env if needed
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+
         SpringApplication.run(DemoApplication.class, args);
     }
 }
