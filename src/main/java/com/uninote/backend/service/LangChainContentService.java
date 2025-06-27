6949b1437971134cfac2d5526e39def62d4c83f7
tasosger <tassos.gerassis@gmail.com>
2025-06-27 17:40:30 +0300

    Set summary temperature to 0.4

--- src/main/java/com/uninote/backend/service/LangChainContentService.java
+++ src/main/java/com/uninote/backend/service/LangChainContentService.java
@@ -98,7 +98,7 @@ public class LangChainContentService {
                 .endpoint(azureConfig.getAzureEndpoint())
                 .apiKey(azureConfig.getAzureApiKey())
                 .deploymentName(azureConfig.getChatDeployment())
-                .temperature(0.7)
+                .temperature(0.4)
                 .build();
     }
     
