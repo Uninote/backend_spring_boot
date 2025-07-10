306cb4599a9637266109be11950b4596169a3057
tasosger <tassos.gerassis@gmail.com>
2025-07-10 19:57:56 +0300

    remove all http tracing logs

--- src/main/java/com/uninote/backend/config/HttpTraceActuatorConfiguration.java
+++ src/main/java/com/uninote/backend/config/HttpTraceActuatorConfiguration.java
@@ -1,15 +1,13 @@
 package com.uninote.backend.config;
 
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HttpTraceActuatorConfiguration {
 
-    @Bean
-    public HttpTraceRepository httpTraceRepository() {
-        return new InMemoryHttpTraceRepository();
-    }
+    // Disabled HTTP tracing to prevent embedding vector logging
+    // @Bean
+    // public HttpTraceRepository httpTraceRepository() {
+    //     return new InMemoryHttpTraceRepository();
+    // }
 }
